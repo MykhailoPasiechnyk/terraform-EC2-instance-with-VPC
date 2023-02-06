cd40572a24abb3a870588c72ccb87185db415f6d
Pasiechnyk Mykhailo <pasiechnyk.ms@gmail.com>
2023-02-06 10:23:38 +0100

    add cloudwatch metric alarm resource for cpu utilization in cloud_watch_cpu_util/main.tf

--- modules/cloud_watch_cpu_util/main.tf
+++ modules/cloud_watch_cpu_util/main.tf
@@ -26,3 +26,24 @@ resource "aws_sns_topic_subscription" "sns_subscription" {
   protocol  = "email"
   endpoint  = var.email
 }
+
+
+resource "aws_cloudwatch_metric_alarm" "cpu_80" {
+  alarm_name                = "ec2-cpu-80"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  actions_enabled           = true
+  alarm_actions             = [aws_sns_topic.sns.arn]
+  ok_actions                = [aws_sns_topic.sns.arn]
+  insufficient_data_actions = []
+
+  dimensions = {
+    InstanceId = var.ec2_id
+  }
+}
--- modules/cloud_watch_cpu_util/variables.tf
+++ modules/cloud_watch_cpu_util/variables.tf
@@ -2,3 +2,8 @@ variable "email" {
   type        = string
   description = "Email to which the notification will be sent"
 }
+
+variable "ec2_id" {
+  type        = string
+  description = "Instance ID"
+}
